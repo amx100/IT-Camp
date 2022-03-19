fec543511dcabf9531aab5d8e1173b57695aa1c5
Ahmed <amx281@gmail.com>
2022-03-19 22:47:17 +0100

    < < > >

---
+++ External W/main.js
@@ -0,0 +1,14 @@
+// typing animation
+
+var typing = new Typed (".typing",{
+
+    strings : ["Designer", "Developer", "Freelancer", "Photographer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+
+});
+
+//sticky navbar
+
+
