f4fcdf0e2d3c899c7e79b2cce7b5c956da876911
Ahmed <amx281@gmail.com>
2022-03-22 02:48:31 +0100

    Project in progress

--- External W/main.js
+++ External W/main.js
@@ -11,4 +11,46 @@ var typing = new Typed (".typing",{
 
 //sticky navbar
 
+window.onscroll =function(){
+
+    var navbar = document.getElementById("navbar");
+
+    if(window.scrollY > 55){
+        navbar.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
+
+
+// counter 
+
+const counters = document.querySelectorAll('.counter');
+const speed = 350; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = Math.ceil(count + inc);
+			// Call function every ms
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
 
