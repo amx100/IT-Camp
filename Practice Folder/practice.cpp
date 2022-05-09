b74dd0d02f3a55eeec2cd8015120e206ed169e67
Ahmed <amx281@gmail.com>
2022-05-09 23:21:24 +0200

    Practice

---
+++ Practice Folder/practice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int funkcija(int a, int b)
+{
+    int x;
+    x = 2 * (a + b);
+    return x;
+}
+
+// int main()
+// {
+
+//     int x;
+//     x = subtraction(5, 1);
+//     cout << "reuslt is: " << x << endl;
+// }
