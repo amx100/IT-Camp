c40aeb49084097dbf2c1d83aac0dde6870a96c9c
Ahned <amx281@gmail.com>
2022-05-10 23:35:52 +0200

    new file

---
+++ Classes/class-28/ind.js
@@ -0,0 +1,45 @@
+/* const obj ={user:'suad', 
+age:29,
+address:{ street:'hotkovo', number:0, city:"Novi Pazar"},};
+
+const {user,age,address} = obj;
+const{street,number,city} = address;
+
+console.log(street);
+ */
+
+/* const [a,b,c,d] = [1,2,3,4,5];
+console.log(a,b,c,d); */
+
+/* class Car {
+
+    constructor(type,price){
+        this.type = type;
+        this.price = price;
+    }
+
+    getPrice(){
+        return this.price;
+    }
+
+    getType(){
+        return this.type;
+    }
+
+    toString(){
+        JSON.stringify(this);
+    }
+
+}
+
+const car1 = new Car ("bmw",13000);
+
+console.log(car1.toString()); */
+
+/* function convertToF(celsius){
+    let fahreneit;
+    return fahreneit;
+}
+
+convertToF(30); */
+
