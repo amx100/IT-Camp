fb4877cb37fda40746672c0d3c23b9b33d8f2e4d
Ahmed <amx281@gmail.com>
2022-05-26 01:35:49 +0200

    asihrnost

---
+++ Classes/class-32/async.js
@@ -0,0 +1,11 @@
+// console.log("hi 1");
+
+const fn = () => {
+    console.log("hi");
+  };
+  
+  setTimeout(() => clearInterval(id), 5005);
+  
+  const id = setInterval(fn, 1000);
+  
+  // console.log("hi 2");
