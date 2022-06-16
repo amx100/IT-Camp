8756a6480603453716660c50fc5ab8ae929d116a
AmX <amx281@gmail.com>
2022-06-16 03:46:14 +0200

    filter

---
+++ Exercise/filter.js
@@ -0,0 +1,11 @@
+// const niz = [24, 06, 2002, "dateofBirth"];
+
+// Niz = Niz.filter((a) => a);
+// console.log(Niz);
+
+// var ahmed = 123;
+
+let ahmed = "to powerfull for this";
+
+ahmed = ahmed.split("");
+console.log(ahmed);
