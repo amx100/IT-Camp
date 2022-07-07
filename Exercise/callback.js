7e3d8f69ce6aeb1a722178f56c0d5c0c607abc5e
AmX <amx281@gmail.com>
2022-07-07 23:35:08 +0200

    callb

--- Exercise/callback.js
+++ Exercise/callback.js
@@ -1,23 +1,23 @@
-const posts = [
-  { title: "Central Intelligence", body: "Dwayne The Rock Johnson" },
-  { title: "Central Intelligence", body: "Dwayne The Rock Johnson" },
-];
+// const posts = [
+//   { title: "Central Intelligence", body: "Dwayne The Rock Johnson" },
+//   { title: "Central Intelligence", body: "Dwayne The Rock Johnson" },
+// ];
 
-function getPost() {
-  setTimeout(() => {
-    let output = "";
-    posts.forEach((post, index) => {
-      output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
-  }, 1000);
-}
+// function getPost() {
+//   setTimeout(() => {
+//     let output = "";
+//     posts.forEach((post, index) => {
+//       output += `<li>${post.title}</li>`;
+//     });
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
 
-function createPost(post, callback) {
-  setTimeout(() => {
-    posts.push(post);
-    callback();
-  }, 2000);
-}
+// function createPost(post, callback) {
+//   setTimeout(() => {
+//     posts.push(post);
+//     callback();
+//   }, 2000);
+// }
 
-createPost({ title: "post Three", body: "This is post three" });
+// createPost({ title: "post Three", body: "This is post three" });
